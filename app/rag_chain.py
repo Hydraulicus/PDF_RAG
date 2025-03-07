bda8e61a30894b47f981fbe63674bbbbac6b4ce1
hydraulicus <852gin@gmail.com>
2025-03-07 10:26:01 +0200

    adding frontend

--- app/rag_chain.py
+++ app/rag_chain.py
@@ -9,6 +9,7 @@ from langchain_community.vectorstores.pgvector import PGVector
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.embeddings import OllamaEmbeddings
+from langchain_core.runnables import RunnableParallel
 from typing_extensions import TypedDict
 
 from config import EMBEDDING_MODEL, PG_COLLECTION_NAME
@@ -45,18 +46,19 @@ class RagInput(TypedDict):
     question: str
 
 final_chain = (
-        {
-            "context": itemgetter("question") | vector_store.as_retriever(),
-            "question": itemgetter("question")
-        }
-        | ANSWER_PROMPT
-        | llm
-        | StrOutputParser()
-               ).with_types(input_type=RagInput)
+        RunnableParallel(
+            context=itemgetter("question") | vector_store.as_retriever(),
+            question=itemgetter("question")
+        ) |
+        RunnableParallel(
+            answer=(ANSWER_PROMPT | llm),
+            docs=itemgetter("context")
+        )
+        ).with_types(input_type=RagInput)
 
 #  Demo in action. Uncomment the following code to see the output
 # FINAL_CHAIN_INVOKE = final_chain.astream_log({
-#     "question": "Why compilers can't easily transform operations into machine instructions rely specific type?"
+#     "question": "Why compilers can't easily transform operations into machine instructions rely on specific type?"
 #     })
 #
 # async def __main__():
